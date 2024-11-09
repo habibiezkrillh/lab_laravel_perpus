8f9edfb9632da0dd65369218664bd11e0f90d40d
habibiezkrillh <zikrillahhabibie@gmail.com>
2024-11-09 16:49:29 +0800

    revisi lagi dan lagi

---
+++ app/Models/Jurnal.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jurnal extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'judul',
+        'penulis',
+        'tahun_terbit',
+        'abstrak',
+        'volume',
+        'nomor',
+        'kategori',
+        'link_akses',
+    ];
+}
---
+++ database/migrations/2024_11_08_022405_create_c_d_s_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('c_d_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('penerbit');
+            $table->string('kategori');
+            $table->year('tahun_rilis');
+            $table->integer('stok');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('c_d_s');
+    }
+};
